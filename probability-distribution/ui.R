8fb5f8fc74ed300699b81de0419f26d86a42ffbd
Adam Sasiadek <Adam Sasiadek>
2017-02-27 14:55:51 +0100

    First version

---
+++ probability-distribution/ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+
+shinyUI(fluidPage(verticalLayout(
+                  
+  fluidRow(
+    align = "center",
+    
+    dataTableOutput("probtable")
+  
+  ),
+  fluidRow(
+    align = "center",
+    
+    sliderInput("probslider",
+                "Proportion of yellow candies in population",
+                value = 0.2,
+                min = 0,
+                max = 1,
+                step = 0.1)
+  )
+)))
