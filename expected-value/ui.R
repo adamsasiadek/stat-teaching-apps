a7eb1f0e84658e5042df74395476081d4235830b
Adam Sasiadek <Adam Sasiadek>
2017-03-13 14:52:39 +0100

    First draft of expected-value app

---
+++ expected-value/ui.R
@@ -0,0 +1,35 @@
+fig.width <- 400
+fig.height <- 150
+
+shinyUI(fluidPage(verticalLayout(
+  fluidRow(
+    align = "center",
+    plotOutput("expectedvalueplot", width = fig.width, height = fig.height)
+  ),
+  fluidRow(
+    align = "center",
+    
+    sliderInput(
+      "probslider",
+      "Proportion of yellow candies in population",
+      value = 0.2,
+      min = 0,
+      max = 1,
+      step = 0.1
+    )
+  ),
+  fluidRow(
+    align = "center",
+    
+    numericInput(
+      "answer",
+      "What number of yellow candies do you expect?",
+      value = 0,
+      min = 0,
+      max = 10,
+      step = 1
+    ),
+    actionButton("submit", "Check answer"),
+    textOutput("answertext")
+  )
+)))
