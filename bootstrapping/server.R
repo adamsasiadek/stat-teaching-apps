283dbfdb4bf70c65aa152794de2cb46950600204
W de Nooy <w.denooy@uva.nl>
2017-03-28 08:27:03 +0200

    Probability (mass) instead of density.

--- bootstrapping/server.R
+++ bootstrapping/server.R
@@ -206,7 +206,7 @@ shinyServer(function(input, output) {
       ggtitle("Proportions of yellow candies in all samples") +
       scale_x_continuous(name = "Proportion of yellow candies",
                         limits = c(-0.2,1))+
-        scale_y_continuous(name = "Density",
+        scale_y_continuous(name = "Probability",
                            limits = c(0,1.1),
                            breaks = seq(0,1,by = 0.2))+
       theme_general()
