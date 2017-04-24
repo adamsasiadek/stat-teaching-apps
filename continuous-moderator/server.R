7ead6c3b35bdf37e552ca5a99916afebc4b30079
Adam Sasiadek <adam.sasiadek@gmail.com>
2017-04-24 13:22:55 +0200

    Minor changes, some comments

--- continuous-moderator/server.R
+++ continuous-moderator/server.R
@@ -1,10 +1,12 @@
 library(shiny)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
- source("../plottheme/styling.R", local = TRUE)
-  n <- 85 
-  # Create predictor.
+  #Load styling for plots
+  source("../plottheme/styling.R", local = TRUE)
+  #CREATE PREDICTOR
+  n <- 85 #number of data points
   set.seed(4932)
   exposure <- runif(n) * 10
   # Create moderator.
@@ -18,25 +20,29 @@ shinyServer(function(input, output) {
   attfun <- function(exposure,contact){
     -0.26*exposure + 0.15*contact + 0.04*exposure*contact + 2
   }
+  ##OUTPUT OF HEADER 
   output$headtext <-renderText(paste("Relationship between Exposure & Attitude with Contact at",
                                              input$modvalueslider))
   ##MAIN PLOT##
   output$mainplot <- renderPlot({
-    df <- data.frame(x = c(-1, 6))
+    #Create data frames for plotting
+    df <- data.frame(x = c(-1, 6)) #Limits for line
     scatter <- data.frame(attitude = attitude, exposure = exposure)
- 
-   ggplot(df, aes(x = x)) +
-     stat_function(
-       fun = attfun,
-       args = list(contact = input$modvalueslider),
-       n = 500) +
-     geom_point(data = scatter, aes(x = exposure, y = attitude)) +
-     coord_cartesian(xlim = c(0, 6), ylim = c(0, 5)) +
-     ylab("Attitude") +
-     xlab("Exposure") +
-     theme_general()
-       
-   })
+    
+    #Plot
+    ggplot(df, aes(x = x)) +
+      stat_function(
+        fun = attfun,
+        args = list(contact = input$modvalueslider),
+        n = 500
+      ) +
+      geom_point(data = scatter, aes(x = exposure, y = attitude)) +
+      coord_cartesian(xlim = c(0, 6), ylim = c(0, 5)) +
+      ylab("Attitude") +
+      xlab("Exposure") +
+      theme_general()
+    
+  })
   ##FORMULA OUTPUT##
   output$formulaui <- renderUI({
     withMathJax(
